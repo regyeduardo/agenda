297a196e28ef5552a442a3bc8b9fb5cbd4706c3f
Regy Eduardo <regy_eduardo@outlook.com>
2019-12-02 10:59:17 -0300

    funcao de mostrar os contatos esta funcionando

--- agenda.py
+++ agenda.py
@@ -12,8 +12,24 @@ def inserir_contato():
     salvar_contato(nome, telefone, email, endereco)
 
 
+def mostrar_contatos():
+    try:
+        with open('contatos.csv', 'r') as file:
+            linhas = file.readlines()
+            for linha in linhas:
+                contatos = linha.strip().split(',')
+                print(f"\nNome: {contatos[0]}")
+                print(f"Telefone: {contatos[1]}")
+                print(f"Email: {contatos[2]}")
+                print(f"Endereço: {contatos[3]}")
+    except Exception as error:
+        print("Algo deu errado")
+        print(error)
+
+
 while True:
-    print("Opção 1 - Inserir Contato")
+    print("\nOpção 1 - Inserir Contato")
+    print("Opção 5 - Mostrar Contatos")
     print("Opção 0 - Sair")
     opcao = input("Digite uma opção: ")
     if opcao == "1":
@@ -21,6 +37,8 @@ while True:
             inserir_contato()
         except:
             print(f"\nContato inserido...")
+    if opcao == "5":
+        mostrar_contatos()
     elif opcao == "0":
         break
     else:
