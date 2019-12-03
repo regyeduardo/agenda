a16a0db780cf3e5dea87df7e8d6ed10d9101614d
Regy Eduardo <regy_eduardo@outlook.com>
2019-12-03 09:21:38 -0300

    Editar contatos e exportar contatos esta funcionando perfeitamente

--- agenda.py
+++ agenda.py
@@ -2,8 +2,11 @@
 import csv
 def salvar_contato(nome, tel, email, ende):
     with open('contatos.csv', 'a') as file:
+            nome = nome.replace(","," ")
+            tel = tel.replace(","," ")
+            email = email.replace(","," ")
+            ende = ende.replace(","," ")
             file.write(f"{nome},{tel},{email},{ende}\n")
-
 
 
 def inserir_contato():
@@ -28,38 +31,32 @@ def mostrar_contatos():
         print("Algo deu errado")
         print(error)
 
-def apagar_contato():
-    contato = input("Qual contato quer apagar? ")
-    contatos = []
+def apagar_contato(contato):
+    contatos = retorna_lista_contatos()
     aux = []
-    with open('contatos.csv','r') as file:
-        for lines in file:
-            contatos.append(lines.strip().split(','))
     
-    ver = False
     for n in contatos:
-        if contato in n[0]:
-            ver = True
+        if contato.lower() == n[0].lower():
+            pass
         else:
             aux.append(n)
     
-    if not ver:
-        print("Contato não encontrado")
-    else:
+    if verifica_existencia_contato(contato):
         with open('contatos.csv', 'w') as file:
-            file.write("") # Precisa do \n para pular uma linha
-        print("Apagado com sucesso")
-    
+            file.write("")
+
         for cont in aux:
             nome = cont[0]
             tel = cont[1]
             email = cont[2]
             endereco = cont[3]
             salvar_contato(nome, tel, email,endereco)
-
-
-def buscar_contato():
-    contato = input("Qual contato quer buscar? ")
+        return aux
+    else:
+        print("Contato não encontrado")
+    
+    
+def verifica_existencia_contato(contato):
     contatos = []
     with open('contatos.csv','r') as file:
         for lines in file:
@@ -67,23 +64,71 @@ def buscar_contato():
     
     ver = False
     for n in contatos:
-        if contato in n[0]:
-            print(f"\nNome: {n[0]}")
-            print(f"Telefone: {n[1]}")
-            print(f"Email: {n[2]}")
-            print(f"Endereço: {n[3]}")
+        if contato.lower() == n[0].lower():
             ver = True
+    
+    if ver:
+        return True
+    else:
+        return False
 
-    if not ver:
-        print("Contato não encontrado")
-        
-        
+def retorna_lista_contatos():
+    contatos = []
+    with open('contatos.csv','r') as file:
+        for lines in file:
+            contatos.append(lines.strip().split(','))
+    
+    return contatos
+
+    
+def buscar_contato(contato):
+    
+    contatos = retorna_lista_contatos()
+
+    if verifica_existencia_contato(contato):
+        for n in contatos:
+            if contato.lower() == n[0].lower():
+                print(f"\nNome: {n[0]}")
+                print(f"Telefone: {n[1]}")
+                print(f"Email: {n[2]}")
+                print(f"Endereço: {n[3]}")
+    else:
+        print("Contato não encontrada")
+
+
+
+def editar_contato(contato):
+    if verifica_existencia_contato(contato):
+        apagar_contato(contato)
+        tel = input(f"Insira o novo tefone: ") 
+        email = input("Insira o novo email: ") 
+        ende = input("Insira o novo endereço: ")
+        salvar_contato(f"{contato[0].upper()+contato[1:]}", tel, email, ende)
+    else:
+        print("Contato não existe")
+
+
+def exportar_contatos():
+    filename = input("Digite o nome do arquivo para exportar: ")
+    contatos = retorna_lista_contatos()
+    with open(f'{filename}.csv', 'w') as file:
+        file.write("")
+    for cont in contatos:
+        with open(f'{filename}.csv', 'a') as file:
+            nome = cont[0].replace(","," ")
+            tel = cont[1].replace(","," ")
+            email = cont[2].replace(","," ")
+            ende = cont[3].replace(","," ")
+            file.write(f"{nome},{tel},{email},{ende}\n")
+    
 
 while True:
     print("\nOpção 1 - Inserir Contato")
+    print("Opção 2 - Editar Contato")
     print("Opção 3 - Apagar Contato")
     print("opção 4 - Buscar Contato")
     print("Opção 5 - Mostrar Contatos")
+    print("Opção 6 - Exportar Contatos")
     print("Opção 0 - Sair")
     opcao = input("Digite uma opção: ")
     if opcao == "1":
@@ -91,12 +136,19 @@ while True:
             inserir_contato()
         except:
             print(f"\nContato inserido...")
+    elif opcao == "2":
+        contato = input("Qual contato quer editar? ")
+        editar_contato(contato)
     elif opcao == "3":
-        apagar_contato()
+        contato = input("Qual contato quer apagar? ")
+        apagar_contato(contato)
     elif opcao == "4":
-        buscar_contato()
+        contato = input("Qual contato quer buscar? ")
+        buscar_contato(contato)
     elif opcao == "5":
         mostrar_contatos()
+    elif opcao == "6":
+        exportar_contatos()
     elif opcao == "0":
         break
     else:
